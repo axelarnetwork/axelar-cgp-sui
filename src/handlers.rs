7a2c3129895200639344059a3ea5b077f85e47ab
patrick <patrickkuo@me.com>
2023-10-17 11:14:11 -0600

    Moving relayer code from Sui to Axelar

---
+++ src/handlers.rs
@@ -0,0 +1,101 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use axum::extract::State;
+use axum::Json;
+use shared_crypto::intent::Intent;
+use sui_keys::keystore::AccountKeystore;
+use sui_sdk::rpc_types::{SuiExecutionStatus, SuiTransactionBlockResponseOptions};
+use sui_sdk::rpc_types::SuiTransactionBlockEffectsAPI;
+use sui_sdk::types::Identifier;
+use sui_sdk::types::programmable_transaction_builder::ProgrammableTransactionBuilder;
+use sui_sdk::types::quorum_driver_types::ExecuteTransactionRequestType;
+use sui_sdk::types::transaction::{
+    CallArg, ObjectArg, Transaction, TransactionData, TransactionKind,
+};
+
+use crate::RelayerState;
+use crate::types::{Error, Input, ProcessCommandsResponse};
+
+// handler for `/process_commands` endpoint
+pub async fn process_commands(
+    State(state): State<RelayerState>,
+    Json(input): Json<Input>,
+) -> Result<ProcessCommandsResponse, Error> {
+    let payload = bcs::to_bytes(&input)?;
+
+    let mut ptb = ProgrammableTransactionBuilder::default();
+    let validator = ObjectArg::SharedObject {
+        id: state.validators,
+        initial_shared_version: state.validators_shared_version.into(),
+        mutable: true,
+    };
+    ptb.move_call(
+        state.gateway_package_id,
+        Identifier::from_str("gateway")?,
+        Identifier::from_str("process_commands")?,
+        vec![],
+        vec![
+            CallArg::Object(validator),
+            CallArg::Pure(bcs::to_bytes(&payload)?),
+        ],
+    )?;
+    let pt = ptb.finish();
+
+    // using read write lock to ensure same coins are not used in multiple tx simultaneously.
+    // todo: this could become performance bottleneck, use coin management to increase throughput if needed.
+    let sui_client = state.sui_client.write().await;
+
+    let dry_run = sui_client
+        .read_api()
+        .dev_inspect_transaction_block(
+            state.signer_address,
+            TransactionKind::ProgrammableTransaction(pt.clone()),
+            None,
+            None,
+        )
+        .await?;
+
+    if let SuiExecutionStatus::Failure { error } = dry_run.effects.status() {
+        return Err(Error::SuiTransactionExecutionFailure(error.clone()));
+    }
+
+    let coins = sui_client
+        .coin_read_api()
+        .get_coins(state.signer_address, None, None, None)
+        .await?
+        .data;
+
+    let coins = coins.into_iter().map(|c| c.object_ref()).collect();
+    let gas_price = sui_client
+        .governance_api()
+        .get_reference_gas_price()
+        .await?;
+
+    let data =
+        TransactionData::new_programmable(state.signer_address, coins, pt, 10000000, gas_price);
+
+    let signature = state
+        .keystore
+        .sign_secure(&state.signer_address, &data, Intent::sui_transaction())
+        .map_err(|e| anyhow!(e))?;
+
+    let resp = sui_client
+        .quorum_driver_api()
+        .execute_transaction_block(
+            Transaction::from_data(data, Intent::sui_transaction(), vec![signature]),
+            SuiTransactionBlockResponseOptions::default(),
+            Some(ExecuteTransactionRequestType::WaitForEffectsCert),
+        )
+        .await?;
+
+    // todo: invoke subsequence contract calls?
+
+    // todo: deal with errors
+    Ok(ProcessCommandsResponse {
+        tx_hash: resp.digest,
+    })
+}
---
+++ src/relayer.rs
@@ -0,0 +1,160 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+
+use axum::Router;
+use axum::routing::post;
+use clap::Parser;
+use futures::future::try_join_all;
+use rxrust::observable::ObservableItem;
+use sui_keys::keystore::{AccountKeystore, InMemKeystore, Keystore};
+use sui_sdk::{SuiClient, SuiClientBuilder};
+use sui_sdk::types::base_types::{ObjectID, SuiAddress};
+use sui_sdk::types::crypto::SignatureScheme;
+use telemetry_subscribers::TelemetryConfig;
+use tokio::sync::RwLock;
+use tokio::task::JoinHandle;
+use tracing::info;
+
+use crate::handlers::process_commands;
+use crate::listener::{
+    ContractCall, OperatorshipTransferred, Subject, SuiAxelarEvent, SuiListener,
+};
+use crate::types::Error;
+
+mod handlers;
+mod listener;
+mod types;
+
+#[derive(Parser)]
+#[clap(
+name = "sui-axelar-relayer",
+rename_all = "kebab-case",
+author,
+version
+)]
+pub struct SuiAxelarRelayer {
+    #[clap(
+    long,
+    env,
+    default_value = "you parade planet become era edit fuel birth arrow cry grunt snow"
+    )]
+    signer_mnemonic: String,
+    #[clap(long, env, default_value = "https://rpc.testnet.sui.io:443")]
+    sui_fn_url: String,
+    #[clap(long, env, default_value = "wss://rpc.testnet.sui.io:443")]
+    sui_ws_url: String,
+    #[clap(long, env, default_value = "127.0.0.1:10000")]
+    listen_address: SocketAddr,
+    #[clap(
+    long,
+    env,
+    default_value = "0x37e8cb0b746891c21f5a09cfc8eaae2c10770e1a367edb57e4b779eed3263314"
+    )]
+    gateway_package_id: ObjectID,
+    #[clap(
+    long,
+    env,
+    default_value = "0x5a444605cda2e01ddee5de4a670be2f20b432b58502c82fae2a3d0a879a8a75f"
+    )]
+    validators: ObjectID,
+    #[clap(long, env, default_value = "896987")]
+    validators_shared_version: u64,
+}
+
+#[derive(Clone)]
+pub struct RelayerState {
+    signer_address: SuiAddress,
+    keystore: Arc<Keystore>,
+    sui_client: Arc<RwLock<SuiClient>>,
+    gateway_package_id: ObjectID,
+    validators: ObjectID,
+    validators_shared_version: u64,
+}
+
+impl SuiAxelarRelayer {
+    pub async fn start(self) -> Result<(), Error> {
+        info!("Starting Sui Axelar relayer");
+
+        info!("Sui Fullnode: {}", self.sui_fn_url);
+        let sui_client = SuiClientBuilder::default()
+            .ws_ping_interval(Duration::from_secs(20))
+            .ws_url(&self.sui_ws_url)
+            .build(&self.sui_fn_url)
+            .await?;
+
+        let mut keystore = Keystore::InMem(InMemKeystore::default());
+        let signer_address =
+            keystore.import_from_mnemonic(&self.signer_mnemonic, SignatureScheme::ED25519, None)?;
+
+        info!("Relayer signer Sui address: {signer_address}");
+
+        let state = RelayerState {
+            signer_address,
+            keystore: Arc::new(keystore),
+            sui_client: Arc::new(RwLock::new(sui_client.clone())),
+            gateway_package_id: self.gateway_package_id,
+            validators: self.validators,
+            validators_shared_version: self.validators_shared_version,
+        };
+
+        let api = self.start_api_service(state).await;
+
+        let (contract_call_handle, contract_call) = self
+            .start_event_listener::<ContractCall>(sui_client.clone(), self.gateway_package_id)
+            .await;
+
+        contract_call.subscribe(|call| {
+            // todo: pass to axelar
+            println!("{call:?}")
+        });
+
+        let (operatorship_transferred_handle, operatorship_transferred) = self
+            .start_event_listener::<OperatorshipTransferred>(sui_client, self.gateway_package_id)
+            .await;
+
+        operatorship_transferred.subscribe(|call| {
+            // todo: pass to axelar
+            println!("{call:?}")
+        });
+
+        try_join_all(vec![
+            api,
+            contract_call_handle,
+            operatorship_transferred_handle,
+        ])
+            .await?;
+        Ok(())
+    }
+
+    async fn start_event_listener<T: SuiAxelarEvent + Clone + 'static>(
+        &self,
+        client: SuiClient,
+        gateway_package_id: ObjectID,
+    ) -> (JoinHandle<()>, Subject<T>) {
+        let sui_listener = SuiListener::new(client, gateway_package_id);
+        let event = Subject::<T>::default();
+        (tokio::spawn(sui_listener.listen(event.clone())), event)
+    }
+
+    async fn start_api_service(&self, state: RelayerState) -> JoinHandle<()> {
+        let app = Router::new()
+            .route("/process_commands", post(process_commands))
+            .with_state(state);
+        let server = axum::Server::bind(&self.listen_address).serve(app.into_make_service());
+        let addr = server.local_addr();
+        let handle = tokio::spawn(async move { server.await.unwrap() });
+
+        info!("Sui Axelar relayer listening on {addr}");
+        handle
+    }
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Error> {
+    let (_guard, _) = TelemetryConfig::new().with_env().init();
+    SuiAxelarRelayer::parse().start().await
+}
