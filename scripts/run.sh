84f13801840c244020c1527361670165038895af
Milap Sheth <milap@interoplabs.io>
2024-06-03 03:54:42 -0400

    ci: pin sui dep version (#45)

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,13 +8,13 @@ fi
 exit_code=0
 
 for module in ./move/*/; do
-    if ! sui move build --lint --warnings-are-errors --path "$module"; then
+    if ! sui move "$1" --lint --warnings-are-errors --path "$module"; then
         exit_code=1
     fi
 done
 
 if [ $exit_code -ne 0 ]; then
     echo ""
-    echo -e "\033[0;31mBuild failed\033[0m"
+    echo -e "\033[0;31m$1 failed\033[0m"
     exit 1
 fi
