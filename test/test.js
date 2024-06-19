1cd9f11374752790f66dcb1838fc261c9cc75b9d
Foivos <foivos@umich.edu>
2024-06-19 15:38:30 +0300

    stash

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const { TxBuilder } =  require('../dist/tx-builder');
 const { SuiClient, getFullnodeUrl } = require('@mysten/sui.js/client');
 
@@ -6,6 +6,7 @@ describe('test', () => {
     let client, keypair;
     before(async() => {
         client = new SuiClient(getFullnodeUrl('localnet'));
+        keypair = new SuiKeypair();
         const builder = new TxBuilder(client);
 
         const response = await builder.signAndExecute(keypair);
