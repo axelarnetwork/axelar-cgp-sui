265f7d5fabe295ad824457c6fbb9ea99ddb2c287
npty <nptytn2@gmail.com>
2024-07-10 17:15:33 +0700

    chore: fix exports in scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,4 @@
 module.exports = {
-    ...require('./gateway'),
-    ...require('./publish-all'),
-    ...require('./utils'),
-    ...require('./governance'),
-    ...require('./publish-package'),
+    ...require('./bcs'),
+    ...require('./tx-builder'),
 };
