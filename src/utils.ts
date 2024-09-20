8d6ae58eb54719fd8b8b5ad96e0f648d3caab5ad
npty <nptytn2@gmail.com>
2024-09-20 14:24:04 +0700

    chore: remove getModuleNameFromSymbol function

--- src/utils.ts
+++ src/utils.ts
@@ -3,44 +3,6 @@ import fs from 'fs';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import toml from 'smol-toml';
 
-export function getModuleNameFromSymbol(symbol: string) {
-    function isNumber(char: string) {
-        return char >= '0' && char <= '9';
-    }
-
-    function isLowercase(char: string) {
-        return char >= 'a' && char <= 'z';
-    }
-
-    function isUppercase(char: string) {
-        return char >= 'A' && char <= 'Z';
-    }
-
-    let i = 0;
-    const length = symbol.length;
-    let moduleName = '';
-
-    while (isNumber(symbol[i])) {
-        i++;
-    }
-
-    while (i < length) {
-        const char = symbol[i];
-
-        if (isLowercase(char) || isNumber(char)) {
-            moduleName += char;
-        } else if (isUppercase(char)) {
-            moduleName += char.toLowerCase();
-        } else if (char === '_' || char === ' ') {
-            moduleName += '_';
-        }
-
-        i++;
-    }
-
-    return moduleName;
-}
-
 export function updateMoveToml(packageName: string, packageId: string, moveDir: string = `${__dirname}/../move`) {
     // Path to the Move.toml file for the package
     const movePath = `${moveDir}/${packageName}/Move.toml`;
