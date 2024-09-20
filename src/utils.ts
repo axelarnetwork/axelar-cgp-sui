bc2861441e99c0fa25c1a32bbc68890e5a038e7b
npty <78221556+npty@users.noreply.github.com>
2024-09-20 14:52:07 +0700

    feat: add get package dependencies (#148)

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
+import path from 'path';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import toml from 'smol-toml';
 
@@ -34,6 +35,30 @@ export function copyMovePackage(packageName: string, fromDir: null | string, toD
     }
 
     fs.cpSync(`${fromDir}/${packageName}`, `${toDir}/${packageName}`, { recursive: true });
+}
+
+/**
+ * Get the local dependencies of a package from the Move.toml file.
+ * @param packageName The name of the package.
+ * @param baseMoveDir The parent directory of the Move.toml file.
+ * @returns An array of objects containing the name and path of the local dependencies.
+ */
+export function getLocalDependencies(packageName: string, baseMoveDir: string) {
+    const movePath = `${baseMoveDir}/${packageName}/Move.toml`;
+
+    if (!fs.existsSync(movePath)) {
+        throw new Error(`Move.toml file not found for given path: ${movePath}`);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { dependencies } = toml.parse(fs.readFileSync(movePath, 'utf8')) as any;
+
+    const localDependencies = Object.keys(dependencies).filter((key: string) => dependencies[key].local);
+
+    return localDependencies.map((key: string) => ({
+        name: key,
+        path: `${baseMoveDir}/${path.resolve(path.dirname(movePath), dependencies[key].local)}`,
+    }));
 }
 
 export const getInstalledSuiVersion = () => {
--- test/utils.js
+++ test/utils.js
@@ -1,28 +1,55 @@
 const { expect } = require('chai');
 const toml = require('smol-toml');
 const fs = require('fs');
-const { updateMoveToml, copyMovePackage } = require('../dist/utils');
+const path = require('path');
+const { updateMoveToml, getLocalDependencies, copyMovePackage } = require('../dist/utils');
 
-describe('UpdateMoveToml', () => {
-    const moveTestDir = `${__dirname}/move`;
+describe('Utils', () => {
+    describe('updateMoveToml', () => {
+        const moveTestDir = `${__dirname}/../move-test`;
 
-    it('should insert published-at and address fields correctly', () => {
-        const testPackageName = 'governance';
-        const testPackageId = '0x01';
+        it('should insert published-at and address fields correctly', () => {
+            const testPackageName = 'governance';
+            const testPackageId = '0x01';
 
-        // Create a new directory for the test package
-        copyMovePackage(testPackageName, undefined, moveTestDir);
+            // Create a new directory for the test package
+            copyMovePackage(testPackageName, undefined, moveTestDir);
 
-        // Update the Move.toml file for the test package
-        updateMoveToml(testPackageName, testPackageId, moveTestDir);
+            // Update the Move.toml file for the test package
+            updateMoveToml(testPackageName, testPackageId, moveTestDir);
 
-        const moveToml = toml.parse(fs.readFileSync(`${moveTestDir}/${testPackageName}/Move.toml`, 'utf8'));
+            const moveToml = toml.parse(fs.readFileSync(`${moveTestDir}/${testPackageName}/Move.toml`, 'utf8'));
 
-        expect(moveToml.package['published-at']).to.equal(testPackageId);
-        expect(moveToml.addresses[testPackageName]).to.equal(testPackageId);
+            expect(moveToml.package['published-at']).to.equal(testPackageId);
+            expect(moveToml.addresses[testPackageName]).to.equal(testPackageId);
+        });
+
+        after(async () => {
+            fs.rmSync(`${moveTestDir}`, { recursive: true, force: true });
+        });
     });
 
-    after(async () => {
-        fs.rmSync(`${moveTestDir}`, { recursive: true, force: true });
+    describe('getLocalDependencies', () => {
+        it('should return the correct dependencies', () => {
+            const testPackageName = 'governance';
+            const baseMoveDir = `${__dirname}/../move`;
+            const dependencies = getLocalDependencies(testPackageName, baseMoveDir);
+
+            expect(dependencies.length).to.greaterThan(0);
+
+            for (const dependency of dependencies) {
+                const dependencyPath = path.resolve(dependency.path, 'Move.toml');
+
+                // Check if the dependency path exists
+                if (fs.existsSync(dependencyPath)) {
+                    const dependencyRaw = fs.readFileSync(dependencyPath, 'utf8');
+
+                    const dependencyJson = toml.parse(dependencyRaw);
+
+                    // Check if the dependency name matches the expected name
+                    expect(dependencyJson.package.name).to.equal(dependency.name);
+                }
+            }
+        });
     });
 });
