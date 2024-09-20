95d5b366cdebe757cf6dbf558cb05fb6160b691a
Foivos <foivos@umich.edu>
2024-09-20 17:06:13 +0300

    added a description of what there is to be done

---
+++ test/its.js
@@ -0,0 +1,10 @@
+/*
+This is a short spec for what there is to be done. You can check https://github.com/axelarnetwork/interchain-token-service/blob/main/test/InterchainTokenService.js for some details.
+[x] Test deployment of interchian token service.
+[x] Test `register_transaction` (this tells relayers how to execute contract calls).
+[x] Test owner functions (mint/burn).
+[x] Test public functions (`register_token` etc.).
+[x] Write an ITS example.
+[x] Use the ITS example for end to end tests.
+*/
+
