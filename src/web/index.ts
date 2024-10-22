b3e8336bca6895456f20f2ed2fad7b00d1547780
npty <nptytn2@gmail.com>
2024-10-22 14:56:25 +0700

    chore: refactor tx-builder

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,4 +2,4 @@ export * from '../common/bcs';
 export * from '../common/utils';
 export * from '../common/types';
 export * from '../common/execute';
-export * from './tx-builder';
+export * from '../common/tx-builder';
