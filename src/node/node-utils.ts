5c4ef14a235c1cbb7908868d548f2c4f7ee5357d
Drew Taylor <drew@chainofinsight.com>
2025-09-26 13:34:36 -0400

    npm run prettier

--- src/node/node-utils.ts
+++ src/node/node-utils.ts
@@ -168,7 +168,6 @@ export function updateMoveToml(
             'latest-published-id': packageId,
             'published-version': String(version + 1),
         };
-
     } else {
         (tomlJson as Record<string, Record<string, string>>).package['published-at'] = packageId;
         (tomlJson as Record<string, Record<string, string>>).addresses[packageName] = packageId;
